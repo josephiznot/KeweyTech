26f2c79a817da479fcf8e8b9c23bd62c1dcb2f3d
Joseph Anderson <josephiznot@gmail.com>
2018-05-07 13:14:13 -0500

    dfasdf

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -289,13 +289,14 @@ class Settings extends Component {
     this.setState({ newPassword: val });
   }
   handleUnlock() {
-    this.state.isAdmin
-      ? this.setState({ locked: false })
-      : Swal({
-          title: "User not an admin",
-          text: "Please login as the admin to edit the settings.",
-          type: "error"
-        });
+    // this.state.isAdmin
+    //   ? this.setState({ locked: false })
+    //   : Swal({
+    //       title: "User not an admin",
+    //       text: "Please login as the admin to edit the settings.",
+    //       type: "error"
+    //     });
+    this.setState({ locked: false });
   }
   handleLock() {
     this.setState({ locked: true });
